b3de55bd407a5eebc87eb8fe50f9ca612fd3c481
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-10-18 23:15:35 +0200

    post commit

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const App = () => {
+const Post = () => {
     return (
         <div>
-            <Posts/>
+            Post
         </div>
     );
 };
 
-export {App};
+export {Post};
