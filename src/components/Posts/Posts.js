6ab9a7dea0c1d2c8f706d2d4e199b8412f0e7897
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-10-18 23:35:11 +0200

    post commit

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Posts = () => {
+
+    const [posts, setPosts]=useState([]);
+    const [postDetails, setPostDetails] = useState(null);
+
+    useEfect ( ()=>{
+
+    },[])
+
     return (
         <div>
             Posts
